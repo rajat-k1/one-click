6a545dd330b60285d1e2eba2c44f7ac113995088
Rajat Kulkarni <rajat.kulkarni017@gmail.com>
2024-05-28 02:00:07 -0700

    fixed a font issue

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import Navigation from "../components/Navigation";
 import "./globals.css";
 import { SocialProvider } from "@/contexts/socialContext";
-import { Inter } from '@next/font/google';
 
 const inter = Inter({ subsets: ["latin"] });
 import Footer from "../components/Footer"; // Import the Footer component
